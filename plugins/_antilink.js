2b4f70abbff54683e007969c81a927284a923609
Ibrrvxhgff <112974692+Ibrrvxhgff@users.noreply.github.com>
2025-08-30 17:27:51 +0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -32,8 +32,8 @@ if (m.originalText.includes(`https://chat.whatsapp.com/${code}`)) return;
 } catch {}
 }
 
-if (!isBotAdmin) return await conn.sendMessage(m.chat, {text: `*「 ANTILINK DETECTADO 」*\n\n${userTag}, enviaste un link pero no puedo eliminarte porque no soy admin.`, mentions: [m.sender]}, { quoted: m });
-await conn.sendMessage(m.chat, { text: `*「 ANTILINK DETECTADO 」*\n\n${userTag}, rompiste las reglas del grupo y serás eliminado.`, mentions: [m.sender] }, { quoted: m });
+if (!isBotAdmin) return await conn.sendMessage(m.chat, {text: `*「 تم اكتشاف رابط 」*\n\n${userTag}، لقد أرسلت رابطًا ولكن لا يمكنني إزالتك لأنني لست مشرفًا.`, mentions: [m.sender]}, { quoted: m });
+await conn.sendMessage(m.chat, { text: `*「 تم اكتشاف رابط 」*\n\n${userTag}، لقد خالفت قوانين المجموعة وسيتم إزالتك.`, mentions: [m.sender] }, { quoted: m });
 try {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
