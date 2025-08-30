5db049ffa4837dc88ef03627c0d8c29d4ee33d2e
Ibrrvxhgff <112974692+Ibrrvxhgff@users.noreply.github.com>
2025-08-30 17:21:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ console.log('Iniciando 🚀🚀🚀')
 import cfonts from 'cfonts';
 import chalk from 'chalk';
 
-cfonts.say('LoliBot-MD', {
+cfonts.say('pain bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta'],
   transition: false
 });
 
-cfonts.say('by: elrebelde21', {
+cfonts.say('by: fete', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta'],
